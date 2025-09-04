dba31d5d3b7c730b1979bcf763e937bc851b6631
Lars <lars.dehairs@gmail.com>
2025-09-04 11:26:30 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sitebox-cache-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // add any CSS/JS/image files you want cached
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
